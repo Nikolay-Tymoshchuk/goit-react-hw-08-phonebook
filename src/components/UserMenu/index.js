640eca88d96682272d2a3a247f60e6a27c484894
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-08-13 01:09:15 +0300

    done forms

---
+++ src/components/UserMenu/index.js
@@ -0,0 +1,10 @@
+<div class="header__logbuttons-list">
+  <button data-log="out" class="header__button-log">
+    <img src="images/svg/log-out.svg" alt="logout logo" />
+    <span>Log out</span>
+  </button>
+  <button data-log="in" class="header__button-log">
+    <img src="images/svg/log-in.svg" alt="login logo" />
+    <span>Log In</span>
+  </button>
+</div>;
