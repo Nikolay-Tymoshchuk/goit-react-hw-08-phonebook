baab20e2062dfcada49c33cf600bcbec0b5f5fab
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-08-15 10:36:57 +0000

    Deploying to gh-pages from @ Nikolay-Tymoshchuk/goit-react-hw-08-phonebook@0697fb1bf6795af0b1c88f42d87e6f22e0e23798 🚀

---
+++ static/js/690.3db2b53c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[690],{690:function(e,o,t){t.r(o),t.d(o,{default:function(){return a}});t(2791);var c=t.p+"static/media/phonebook.be5110e353b7785f26c7d4c8ce7369d9.svg",n="Greeting_image__++kjI",i="Greeting_title__V5zVT",s=t(184);function a(){return(0,s.jsxs)("section",{children:[(0,s.jsx)("h1",{className:i,children:"Welcome to the contact book"}),(0,s.jsx)("img",{className:n,src:c,alt:"phonebook icon"})]})}}}]);
+//# sourceMappingURL=690.3db2b53c.chunk.js.map
