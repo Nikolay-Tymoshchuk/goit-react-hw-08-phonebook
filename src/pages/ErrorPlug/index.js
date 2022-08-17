b80aa682066bfa67e3c1b950d02e8faae1c8e440
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-08-17 23:05:50 +0300

    add panda

--- src/pages/ErrorPlug/index.js
+++ src/pages/ErrorPlug/index.js
@@ -1,8 +1,14 @@
 import wonderWError from 'assets/wonderW.svg';
+import styles from './index.module.scss';
 export default function ErrorPlug() {
   return (
-    <div>
-      <img src={wonderWError} alt="Wonder Worker customed error page" />
-    </div>
+    <section className={styles.section}>
+      <h2 className="visually-hidden">Page not found</h2>
+      <img
+        className={styles.panda}
+        src={wonderWError}
+        alt="Wonder Worker customed error page"
+      />
+    </section>
   );
 }
